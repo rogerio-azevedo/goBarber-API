555228dd18e70845c824b8621bf567efaa9bfa22
Rogerio Azevedo <rogerioazevedo@pop-os.localdomain>
2019-07-10 23:13:27 -0400

    Hash Pass + List Users

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,27 @@
+import User from '../models/User';
+
+class UserController {
+  async index(req, res) {
+    const list = await User.findAll();
+
+    return res.json(list);
+  }
+
+  async store(req, res) {
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+
+    if (userExists) {
+      return res.status(400).json({ error: 'User alredy exists!' });
+    }
+    const { id, name, email, provider } = await User.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
+}
+
+export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Rogerio Azevedo',
-    email: 'rogerio@rogerioazevedo.net',
-    password_hash: '12345678',
-  });
-
-  return res.json(user);
-});
+routes.get('/users/index', UserController.index);
+routes.post('/users', UserController.store);
 
 export default routes;
