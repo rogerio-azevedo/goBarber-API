78be6789b6e57efcb25916eb53676d9e1a4ceea7
Rogerio Azevedo <rogerioazevedo@pop-os.localdomain>
2019-07-11 16:59:31 -0400

    Validacao

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,12 @@ import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.get('/users/index', UserController.index);
+routes.post('/sessions', SessionController.store);
 routes.post('/users', UserController.store);
 
 routes.use(authMiddleware);
 
 routes.put('/users/update', UserController.update);
-routes.post('/sessions', SessionController.store);
+routes.get('/users/index', UserController.index);
 
 export default routes;
