685aba227e360f50c5da112c63568d5731397c49
Rogerio Azevedo <rogerioazevedo@pop-os.localdomain>
2019-07-12 16:32:02 -0400

    Add Multer

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
 routes.post('/users', UserController.store);
@@ -12,5 +15,9 @@ routes.use(authMiddleware);
 
 routes.put('/users/update', UserController.update);
 routes.get('/users/index', UserController.index);
+
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ ok: true });
+});
 
 export default routes;
