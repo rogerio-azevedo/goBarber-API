40d4c8278701298bc793d29bb53361a1fde36328
Rogerio Azevedo <rogerioazevedo@pop-os.localdomain>
2019-07-13 10:57:18 -0400

    List Appoint + Pagination

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import ProviderController from './app/controllers/ProviderController';
 import FileController from './app/controllers/FileController';
+import AppointmentController from './app/controllers/AppointmentController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -18,6 +19,8 @@ routes.use(authMiddleware);
 routes.put('/users/update', UserController.update);
 routes.get('/users/index', UserController.index);
 routes.get('/providers', ProviderController.index);
+routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
