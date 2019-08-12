7c4104bad2eb308f83bfedd761ff02ad220aaac0
Rogerio Azevedo <rogerioazevedo@pop-os.localdomain>
2019-08-11 21:32:59 -0400

    Ajustes API

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,5 +1,6 @@
 import * as Yup from 'yup';
 import User from '../models/User';
+import File from '../models/File';
 
 class UserController {
   async index(req, res) {
@@ -76,13 +77,23 @@ class UserController {
       return res.status(401).json({ error: 'Password doesnt match!' });
     }
 
-    const { id, name, provider } = await user.update(req.body);
+    await user.update(req.body);
+
+    const { id, name, avatar } = await User.findByPk(req.userId, {
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
 
     return res.json({
       id,
       name,
       email,
-      provider,
+      avatar,
     });
   }
 }
